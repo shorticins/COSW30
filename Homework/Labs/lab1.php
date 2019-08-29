3c45eda0817708b7cc13d34a8765ea25fc4ef2cb
EC2 Default User <ec2-user@ip-172-31-91-76.ec2.internal>
2019-08-29 02:54:54 +0000

    Created Lab 1 Initial Info

---
+++ Homework/Labs/lab1.php
@@ -0,0 +1,24 @@
+
+<!Doctype html>
+<html>
+    <head>
+        
+        <title>My First PHP Page</title>
+        <style>
+            
+        </style>
+        
+    </head>
+    
+    <body>
+        
+        <h1>Shanen Dibbern</h1>
+        <p>Lorem ipsum dolor amet pok pok affogato portland, vaporware blog seitan sriracha pabst. Vegan schlitz vice, deep v blue bottle shabby chic chicharrones fanny pack ramps YOLO synth subway tile umami cardigan. Cronut skateboard blog pabst, post-ironic forage polaroid vexillologist small batch normcore aesthetic etsy. Sustainable VHS kombucha shabby chic typewriter, venmo hashtag pug humblebrag poutine hell of scenester locavore vaporware. Chartreuse deep v gentrify taxidermy pinterest four dollar toast hell of keytar banjo kogi la croix poutine schlitz kale chips air plant. </p>
+        
+        <?php
+        echo 'This is my first PHP paragraph'
+        ?>
+    </body>
+    
+    
+</html>
