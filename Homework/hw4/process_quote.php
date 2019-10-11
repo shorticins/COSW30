cf694c4b4c728a09ee1aad0342897208c4f8051f
shorticins <shanendibbern@gmail.com>
2019-10-11 04:33:42 +0000

    hw7 final

--- Homework/hw4/process_quote.php
+++ Homework/hw4/process_quote.php
@@ -5,11 +5,6 @@
         <title>Array Outputs</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://fonts.googleapis.com/css?family=Raleway|Turret+Road&display=swap" rel="stylesheet">
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> f5bd9a9ff53f1bf212f42726634e5aed5bdaf8e3
         <style>
             body{
                 padding: 0;
@@ -27,14 +22,6 @@
                 font-family: 'Turret Road', cursive;
                 font-size: 50px;
             }
-<<<<<<< HEAD
-        </style>
-
-    </head>
-
-    <body>
- <header>
-=======
             #tables {
                 display: block;
                 padding: 5px;
@@ -42,12 +29,11 @@
                 margin: auto;
             }
         </style>
-        
+
     </head>
-    
+
     <body>
- <header> 
->>>>>>> f5bd9a9ff53f1bf212f42726634e5aed5bdaf8e3
+ <header>
           <h1> Fun with Arrays</h1>
         </header>
 
@@ -61,7 +47,6 @@ $quote_explode = explode(' ', $quote);
 
 if(!empty($quote)) {
     echo "<h2>Your original list is:</h2>";
-<<<<<<< HEAD
 
     foreach($quote_explode as $value) {
 
@@ -91,113 +76,54 @@ if(!empty($quote)) {
    foreach($quote_explode as $key => $value){
        echo "<p>$value</p>";
    }
-   echo "br";
-   //add words to array
-   "<h2>Your list with three words added is:</h2>";
-  $quote_explode[] = "Because";
-  $quote_explode[] = "I'm";
-  $quote_explode[] = "Cool";
-
-  foreach($quote_explode as $key => $value){
-      echo "<p>$value</p>";
-  }
-
-  echo "<p>Your new total word count is:</p>";
-  echo "<p>" . count($quote_explode) . "</p>";
-
-}
-
-
-
-
-=======
-    
-    foreach($quote_explode as $value) {
-    
-    echo "<p>$value</p>";
-}
-     echo "<p><strong>The total word count of your list is:</strong></p>";
-     
-     echo "<p>" . count($quote_explode) . "</p>";
-     
-     echo "<br>";
-     
-     echo "<h2>Your list in alphabetical order is:</h2> <br>";
-     
-     sort($quote_explode);
-     
-      foreach($quote_explode as $key => $value) {
-    
-   echo "<p>$value</p>";
-      }
    echo "<br>";
-   
-   
-   echo "<h2>Your list in reverse alphabetical order is:</h2>";
-   
-   rsort($quote_explode);
-   
-   foreach($quote_explode as $key => $value){
-       echo "<p>$value</p>";
-   }
-   echo "<br>";
-   
-   
+
+
    //add words to array
-   
+
    echo "<h2>Your list with three words added is:</h2>";
    $quote_explode = explode(' ', $quote);
   $quote_explode[] = "Because";
   $quote_explode[] = "I'm";
   $quote_explode[] = "Cool";
-  
+
   foreach($quote_explode as $key => $value){
       echo "<p>$value</p>";
   }
-  
+
   echo "<p><strong>Your new total word count is:<strong></p>";
   echo "<p>" . count($quote_explode) . "</p>";
-  
+
   echo "<br>";
-  
+
   $removed_index = array_slice($quote_explode, 3);
-  
+
   echo "<h2>Your list after removing the first 3 items is:</h2>";
-  
+
   foreach($removed_index as $key => $value){
       echo "<p>$value</p>";
   }
-  
-  echo "<p><strong>Your word count is now:</strong></p>";
-  
-  echo count($removed_index);
-  
-  echo "<br>";
-  
-}
-    
-    
-    
-    
->>>>>>> f5bd9a9ff53f1bf212f42726634e5aed5bdaf8e3
 
-     //echo "<p>" . $alphabetize . "</p>
+  echo "<p><strong>Your word count is now:</strong></p>";
+
+  echo count($removed_index);
+
+  echo "<br>";
+
+}
+
+    //echo "<p>" . $alphabetize . "</p>
 
 
     else{
         echo "<p>Please enter 5 words separated by a space.</p>";
     }
-
-
-
 ?>
-<<<<<<< HEAD
-=======
+
 <p>Return to <a href="quote.html">form</a></p>
 <p>~OR~</p>
 <form action="population.php" method="POST">
 <button id="tables" type="submit">Go to Array Tables</button>
 </form>
->>>>>>> f5bd9a9ff53f1bf212f42726634e5aed5bdaf8e3
 </body>
 </html>
--- Homework/hw7/function_machine.php
+++ Homework/hw7/function_machine.php
@@ -1,25 +1,107 @@
-
 <?php
- function addtwo($a = 0, $b = 0)
+ function addthree($a = 0, $b = 0, $c = 0)
  {
- return ($a + $b);
+ return ($a + $b + $c);
  }
+
  $value1 = $_POST['val1'];
  $value2 = $_POST['val2'];
+ $value3 = $_POST['val'];
+ $random = rand(1,1000)
 ?>
-<html>
 
- <title>PHP - Add two numbers</title>
+<html>
+<head>
+<meta charset="utf-8">
+ <title>PHP - Function Machine</title>
+ <style>
+body{
+    background-image: url("gears.gif");
+ background-color: #cccccc;
+ background repeat: no-repeat;
+ background-size: cover;
+ margin: 0;
+ padding: 0;
+ font-size: 25px;
+}
+header{
+    margin: 0;
+    padding: 50px 0;
+    background-color: cornflowerblue;
+}
+h1 {
+    text-align: center;
+}
+
+  #form-outer {
+  background-color: rgb(250, 250, 250);
+  margin: 75px auto;
+  border: 5px groove cornflowerblue;
+  border-radius: 4px;
+  width: 75%;
+  max-width: 900px;
+  padding: 10px;
+  padding-top: 15px;
+}
+form{
+    width: 50%;
+    margin: auto;
+}
+h2{
+    text-align: center;
+}
+p{
+    color: #000;
+    font-weight: 8;
+    text-align:center;
+}
+ </style>
+ </head>
+
  <body>
+ <header>
+<h1>Function Machine</h1>
+ </header>
+
+
+ <div id="form-outer">
+  <h2>Enter 3  Numbers To Be Calculated</h2>
+  <p>(hint:each form operates independent of each other. Once calculations are complete, you can enter new numbers to calculate)</p>
  <form action="function_machine.php" method="post">
- <input type="text" name="val1" value="0" />
- <input type="text" name="val2" value="0" />
- <input type="submit" value="Calculate values"/>
+ <input type="text" name="val1" value="0" placeholder=""/>
+ <input type="text" name="val2" value="0" placeholder=""/>
+ <input type="text" name="val" value="0" placeholder="">
+ <input type="submit" value="Add"/>
  </form>
+<div>
 
  <?php
- echo $value1 . " + " . $value2 . " = ";
- echo addtwo($value1+$value2);
+ echo "<p>Sum of your numbers + a random number:</p>";
+echo "<p>" . $value1 . '+'. $value2 . '+' . $value3 . '+' . $random . '=' . addthree($value1+$value2+$value3+$random) . "</p>";
+ echo "<p>" . 'Square root of your answer = ' . sqrt(addthree($value1+$value2+$value3+$random)) . "<p/>";
+ echo "<p>Enter new numbers for a different result."
  ?>
+<?php
+ function bcmul($value1, $value2){
+
+ return ($value1 * $value2);
+ }
+ $value1 = $_POST['val3'];
+ $value2 = $_POST['val4'];
+?>
+ <div id="form-outer">
+ <h2>Enter 2 Numbers to be calculated</h2>
+ <form action="function_machine.php" method="post">
+ <input type="text" name="val3" value="" placeholder="0"/>
+ <input type="text" name="val4" value="" placeholder="0"/>
+ <input type="submit" value="Multiply"/>
+  <?php
+
+echo "<p>" . 'Answer:' . $value1 . 'x'. $value2 . '=' . bcmul($value1, $value2) . "</p>";
+echo "<p>" . 'Square root of you answer = ' . sqrt(bcmul($value1, $value2)) . "</p>";
+echo "<p>Enter new numbers for a different result."
+ ?>
+ </form>
+<div>
  </body>
 </html>
